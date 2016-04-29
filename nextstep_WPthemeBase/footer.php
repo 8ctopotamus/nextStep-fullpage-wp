d148cead1373d6345b37154f0daca5b534686c88
Josh Naylor <Josh Naylor>
2016-04-29 14:37:49 -0500

    First commit

---
+++ nextstep_WPthemeBase/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package nextStep
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'nextstep' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'nextstep' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'nextstep' ), 'nextstep', '<a href="https://icshelpsyou.com" rel="designer">In-house Computer Services, LLC</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
